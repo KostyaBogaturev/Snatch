5c826007ee46e969af311975e2d39ceda1cd741a
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-01-18 14:54:12 +0200

    Added class for converting Json

---
+++ snatch/Helper/MonoRate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace snatch.Helper
+{
+
+    public class MonoRate
+    {
+        [JsonProperty("curentCodeA")]
+        public int CodeOtherCurrency { get; set; }
+        [JsonProperty("curentCodeB")]
+        public int CodeUAH { get; set; }
+        [JsonProperty("curentCodeA")]
+        public long Date { get; set; }
+        [JsonProperty("rateBuy")]
+        public double RateBuy { get; set; }
+        [JsonProperty("rateSell")]
+        public double RateSell { get; set; }
+    }
+}
